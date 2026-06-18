5dca82dad407bf97cb60b34cc77bd1dc8efeed9b
23154ja <23154ja@hvhs.school.nz>
2026-06-19 10:00:26 +1200

    added logout

--- databaseAssets/javaScript/firebaseLogin.js
+++ databaseAssets/javaScript/firebaseLogin.js
@@ -1,4 +1,7 @@
+
 function googleLoginRequest() {
+    console.log('');
+    console.log('runing func, googleLoginRequest');
   
 if(!isLoggedInCheck()) {
          googleLoginPopup();
@@ -21,16 +24,45 @@ function isLoggedInCheck() {
 
 
 function googleLoginPopup() {
+    console.log('');
+    console.log('runing func, googleLoginPopup');
+
   let provider = new firebase.auth.GoogleAuthProvider();
+
+  console.log('attempting to log in')
 
   firebase.auth().signInWithPopup(provider).then((result) => {
 
     console.log('user has logged in');
-
+console.log(result)
   });
 }
 
 
-firebase.auth().onAuthStateChanged((_user) => {
+function googleLogoutRequest() {
+  firebase.auth().signOut();
+  console.log('user has logged out');
+}
+
+
+
+    firebase.auth().onAuthStateChanged((_user) => {
     GLOBAL_user = _user
+    two();
 });
+
+function two() {
+if(isLoggedInCheck) {
+
+} else {
+  document.getElementById('greetingText').value
+}
+}
+
+
+function logError(errorMessage) {
+  console.log("");
+  console.log('their was an error: ');
+  console.log(errorMessage);
+
+}
